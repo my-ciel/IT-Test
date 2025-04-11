85bd882c58d3b69c8eb5790f34c5b5e6f17e0f8d
my-ciel <112933863+my-ciel@users.noreply.github.com>
2025-04-11 10:51:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,16 +5,6 @@ echo "Updating package list and installing Python3..."
 sudo apt update -y
 sudo apt install python3 python3-pip -y
 
-echo "Checking Python version..."
-python3 --version
-
-# Git Install and Clone
-echo "Installing Git..."
-sudo apt install git -y
-
-echo "Cloning the IT-Test repository..."
-git clone https://github.com/my-ciel/IT-Test.git
-
 # Moving Lab files 
 echo "Moving unit_test and integration_test folders to /home/ubuntu..."
 mv IT-Test/unit_test /home/ubuntu/
@@ -23,4 +13,7 @@ mv IT-Test/test /home/ubuntu/
 mv IT-Test/00-installer-config.yaml /home/ubuntu/.
 
 echo "Setup complete!"
+echo "Checking Python version..."
+python3 --version
+
 
